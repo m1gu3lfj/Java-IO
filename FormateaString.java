527d7dcac78f5140dd0081a495ef685c3b44e898
Miguel Flores Jiménez <50829507+m1gu3lfj@users.noreply.github.com>
2020-01-02 22:28:39 +0100

    Update FormateaString.java

--- FormateaString.java
+++ FormateaString.java
@@ -5,9 +5,9 @@ public class FormateaString {
 	public static void main(String[] args) {
 		String o = "raíz cuadrada";
 		int i = 2;
-        double r = Math.sqrt(i);
+        	double r = Math.sqrt(i);
         
-        System.out.println("La "+ o +" de " + i + " es " + r);
-        System.out.format("La %s de %d es %f%n", o, i, r);
-    }
+        	System.out.println("La "+ o +" de " + i + " es " + r);
+        	System.out.format("La %s de %d es %f%n", o, i, r);
+    	}
 }
